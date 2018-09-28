b82cc814675c449275cb5b1471eef0c8b419cb5f
GrimadhaPratamaAji <43518826+GrimadhaPratamaAji@users.noreply.github.com>
2018-09-28 10:06:42 +0700

    Update formawal.php
    Grimadha Pratama Aji-6701174125-D3 mi 4104

--- formawal.php
+++ formawal.php
@@ -1,5 +1,11 @@
-<?php
+
+<form action="PROSES.php" method="post">
+ 	username: <input type="text" name="username"><br><br>
+
+ 	password: <input type="password" name="password"><br><br>
+
+ 	<input type="submit" value="masuk">
+ </form>
 
 
 
-?>
